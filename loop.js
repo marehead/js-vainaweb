25f186237dfe8a271232689e6ba5ad9656e00bf5
HeyMaroka <marisantl@hotmail.com7>
2022-01-19 07:33:48 -0300

    first

---
+++ loop.js
@@ -0,0 +1,31 @@
+/*
+for (let x = 1; x <= 10; x += 1) {
+    console.log(x)
+}
+*/
+
+
+
+/*
+var cont = 10;
+while (cont >= 1) {
+    console.log(cont);
+    cont--;
+}
+*/
+
+
+/*
+for (let x = 1; x <= 100; x += 2) {
+    console.log(x)
+}
+*/
+
+
+
+
+/*
+for (let x = 0; x <= 100; x += 2) {
+    console.log(x)
+}
+*/
